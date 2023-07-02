c573ce36700af0cdde56c093f7729521ced1b2d6
zouyuxi <zouyx@knownsec.com>
2023-07-02 09:22:03 +0800

    支持中文匹配

--- trie.go
+++ trie.go
@@ -11,7 +11,6 @@ type Node struct {
 	children map[rune]*Node   // 使用 map 存储叶子节点,key:'char' ,value: *Node
 	exist    map[int]struct{} // 关键词节点是一个完整的匹配词，记录其长度
 	failure  *Node            // 记录失败指针
-	key      string
 }
 
 func newNode() *Node {
